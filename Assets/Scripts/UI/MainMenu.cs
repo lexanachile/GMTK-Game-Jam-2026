27ea806903db8ac8e951c7b079b63832ca2e2a3f
lexanachile <yarashsei@gmail.com>
2026-07-25 01:13:15 +0200

    add main menu prototype

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("SampleScene"); // имя твоей игровой сцены (без расширения)
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+}
